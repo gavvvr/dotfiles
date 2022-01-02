a3c13466fd28899b4c09b34e0489fc553110f538
Kirill Gavrilov <gavvvr@ya.ru>
2022-01-02 20:33:41 +0300

    Disable shortcut to turn on Mac dictation

--- .macos-essentials.sh
+++ .macos-essentials.sh
@@ -2,3 +2,6 @@ defaults write com.apple.dock orientation left
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock persistent-apps -array
 killall Dock
+
+# Disable shortcut (double-fn by default) for enabling dictation
+defaults write com.apple.HIToolbox AppleDictationAutoEnable -int 0

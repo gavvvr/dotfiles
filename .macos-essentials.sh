a6ca7bc65515dda337e388dfc7e2407dcfea0d8c
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2022-10-26 03:25:45 +0300

    Disable Monterey's Quick Notes hot corner

--- .macos-essentials.sh
+++ .macos-essentials.sh
@@ -1,6 +1,9 @@
 defaults write com.apple.dock orientation left
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock persistent-apps -array
+
+defaults write com.apple.dock wvous-br-modifier -int 1
+
 killall Dock
 
 # Disable shortcut (double-fn by default) for enabling dictation

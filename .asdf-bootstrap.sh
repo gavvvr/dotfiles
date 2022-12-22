152bc8ce7f77fb166cb0e41436902c1d1e33b899
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2022-12-22 15:10:10 +0300

    Simplify asdf bootstrap script

--- .asdf-bootstrap.sh
+++ .asdf-bootstrap.sh
@@ -1,14 +1,11 @@
 ASDF_DIR=~/.asdf; git clone https://github.com/asdf-vm/asdf.git $ASDF_DIR \
 && git -C $ASDF_DIR checkout `git -C $ASDF_DIR describe --abbrev=0 --tags`
+source $ASDF_DIR/asdf.sh
 
 asdf plugin add ruby
-
-
-asdf plugin add nodejs
-bash -c '${ASDF_DIR}/plugins/nodejs/bin/import-release-team-keyring'
-
 asdf install ruby latest
+asdf global ruby latest
 
 # sudo apt install -y curl dirmngr gpg
-# brew install gpg coreutils
-asdf install nodejs latest
+asdf plugin add nodejs
+asdf install nodejs lts
